4d05c8885d10f828f9d481ad4a47e078be2329e6
Justica <atsuronreplaced@gmail.com>
2024-07-10 23:22:21 +0900

    Add GetEndSeq method and its test

--- cells/field_cell.go
+++ cells/field_cell.go
@@ -1,6 +1,9 @@
 package cells
 
-import "github.com/freecellsolver/cards"
+import (
+	"github.com/freecellsolver/cards"
+	"slices"
+)
 
 type FieldCell struct {
 	CardStack []cards.Card
@@ -16,4 +19,35 @@ func (fCell FieldCell) CanPlace(card cards.Card) bool {
 		return false
 	}
 	return card.GetNumCode() == fCell.CardStack[len(fCell.CardStack)-1].GetNumCode()-uint8(1)
+}
+
+func (fCell FieldCell) GetEndSeq() []cards.Card {
+	if len(fCell.CardStack) == 0 {
+		return []cards.Card{}
+	}
+
+	seq := make([]cards.Card, 0, len(fCell.CardStack))
+	reversed := slices.Clone(fCell.CardStack)
+	slices.Reverse(reversed)
+	for _, card := range reversed {
+		if len(seq) == 0 {
+			seq = append(seq, card)
+			continue
+		}
+
+		currentSeqRoot := seq[len(seq)-1]
+		if currentSeqRoot.IsBlack() == card.IsBlack() {
+			// If same color, end sequence
+			break
+		}
+
+		if currentSeqRoot.GetNumCode()+1 != card.GetNumCode() {
+			// If discontinued number, end sequence
+			break
+		}
+		seq = append(seq, card)
+	}
+
+	slices.Reverse(seq)
+	return seq
 }
--- cells/field_cell_test.go
+++ cells/field_cell_test.go
@@ -2,6 +2,7 @@ package cells
 
 import (
 	"github.com/freecellsolver/cards"
+	"reflect"
 	"testing"
 )
 
@@ -72,6 +73,123 @@ func TestFieldCell_CanPlace(t *testing.T) {
 			}
 			if got := fCell.CanPlace(tt.args.card); got != tt.want {
 				t.Errorf("CanPlace() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestFieldCell_GetEndSeq(t *testing.T) {
+	type fields struct {
+		CardStack []cards.Card
+	}
+	tests := []struct {
+		name   string
+		fields fields
+		want   []cards.Card
+	}{
+		{
+			name: "Empty stack: return empty",
+			fields: fields{
+				CardStack: []cards.Card{},
+			},
+			want: []cards.Card{},
+		},
+		{
+			name: "Single stack: return single card",
+			fields: fields{
+				CardStack: []cards.Card{
+					{uint8(1)},
+				},
+			},
+			want: []cards.Card{
+				{uint8(1)},
+			},
+		},
+		{
+			name: "Continued and same color stack: return last",
+			fields: fields{
+				CardStack: []cards.Card{
+					{uint8(6)},
+					{uint8(5)},
+					{uint8(4)},
+					{uint8(3)},
+				},
+			},
+			want: []cards.Card{
+				{uint8(3)},
+			},
+		},
+		{
+			name: "Discontinued and same color stack: return last",
+			fields: fields{
+				CardStack: []cards.Card{
+					{uint8(6)},
+					{uint8(9)},
+					{uint8(1)},
+					{uint8(3)},
+				},
+			},
+			want: []cards.Card{
+				{uint8(3)},
+			},
+		},
+		{
+			name: "Continued and alternation color stack: return all",
+			fields: fields{
+				CardStack: []cards.Card{
+					{uint8(6)},
+					{uint8(21)},
+					{uint8(4)},
+					{uint8(19)},
+				},
+			},
+			want: []cards.Card{
+				{uint8(6)},
+				{uint8(21)},
+				{uint8(4)},
+				{uint8(19)},
+			},
+		},
+		{
+			name: "Discontinued and alternation color stack: return last",
+			fields: fields{
+				CardStack: []cards.Card{
+					{uint8(22)},
+					{uint8(41)},
+					{uint8(17)},
+					{uint8(35)},
+				},
+			},
+			want: []cards.Card{
+				{uint8(35)},
+			},
+		},
+		{
+			name: "Partial continued and alternation color stack: return the part",
+			fields: fields{
+				CardStack: []cards.Card{
+					{uint8(45)}, // ♣K
+					{uint8(10)}, // ♠10
+					{uint8(18)}, // ♥2
+					{uint8(39)}, // ♣7
+					{uint8(54)}, // ♦6
+					{uint8(5)},  // ♠5
+				},
+			},
+			want: []cards.Card{
+				{uint8(39)}, // ♣7
+				{uint8(54)}, // ♦6
+				{uint8(5)},  // ♠5
+			},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			fCell := FieldCell{
+				CardStack: tt.fields.CardStack,
+			}
+			if got := fCell.GetEndSeq(); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("GetEndSeq() = %v, want %v", got, tt.want)
 			}
 		})
 	}
